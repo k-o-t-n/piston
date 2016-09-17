4a902294baaac4e38c308a5c841118586754d486
David Raine <daveraine@users.noreply.github.com>
2016-09-17 13:38:03 +0100

    Add custom error pages

--- src/Piston/App_Start/RouteConfig.cs
+++ src/Piston/App_Start/RouteConfig.cs
@@ -9,6 +9,15 @@
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("Error403", "errors/403",
+                new { controller = "Errors", action = "AccessDenied" });
+
+            routes.MapRoute("Error404", "errors/404",
+                new { controller = "Errors", action = "NotFound" });
+
+            routes.MapRoute("Error500", "errors/500",
+                new { controller = "Errors", action = "InternalError" });
+
             routes.MapMvcAttributeRoutes();
         }
     }
---
+++ src/Piston/Controllers/ErrorsController.cs
@@ -0,0 +1,31 @@
+﻿namespace Piston.Controllers
+{
+    using System.Web.Mvc;
+
+    public class ErrorsController : Controller
+    {
+        [HttpGet]
+        public ActionResult Error()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult InternalError()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
